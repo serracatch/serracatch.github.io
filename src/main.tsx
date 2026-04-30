7fe87d0644ba82e05764a7992bb1680c77ff1c77
SerraCatch <serracatch@gmail.com>
2026-04-30 11:03:56 -0400

    x

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import HomePage from './pages/HomePage';
 
-createRoot(document.getElementById('root')).render(
+hydrateRoot(
+  document.getElementById('root')!,
   <StrictMode>
     <HashRouter>
       <Routes>
